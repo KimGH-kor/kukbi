f80546b94b239b1c795dbb9ca4c1f88bfce641dc
KimGH-kor <socoolq@naver.com>
2021-08-18 17:11:30 +0900

    1

---
+++ chapter01/src/ch01/Hello.java
@@ -0,0 +1,10 @@
+package ch01;
+
+public class Hello {
+	public static void main(String[] args) {
+		int num = 321;
+		
+		System.out.println(num%10);
+
+	}
+}
---
+++ chapter01/src/ch01/tutor.java
@@ -0,0 +1,21 @@
+package ch01;
+
+public class tutor {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		int num = 10;
+		
+		while(num != 0) {
+			System.out.println(num);
+			num--;
+			if(num%2 == 0) {
+				System.out.println(num);
+			}
+		}
+		
+		num = 10;
+		
+
+	}
+}
